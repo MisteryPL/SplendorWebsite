4a6376c644a0f4109401065c645032f87436006f
MisteryPL <43848600+MisteryPL@users.noreply.github.com>
2018-11-22 20:20:16 +0100

    Delete ranking.php

--- skrypty/ranking.php
+++
@@ -1,7 +0,0 @@
-<?php
-$tytul = "Ranking";
-$baner = '<div id="baner"></br>Ranking</div><br />';
-$zawartosc = '<div id="tresc">
-            Strona poswiecona rankingowi
-              </div>';
-?>
