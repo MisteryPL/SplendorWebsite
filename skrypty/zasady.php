9ac83b8bdd62c95a4c7869f0ba0e75d77a44345f
MisteryPL <43848600+MisteryPL@users.noreply.github.com>
2018-11-22 20:17:46 +0100

    Delete zasady.php

--- skrypty/zasady.php
+++
@@ -1,7 +0,0 @@
-<?php
-$tytul = "Zasady gry";
-$baner = '<div id="baner"></br>Zasady gry</div><br />';
-$zawartosc = '<div id="tresc">
-            Strona poswiecona zasadom gry
-              </div>';
-?>
